0ac45abb574db3f2798bf909acfbcaf32ffb8348
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-07-20 15:59:24 -0400

    created Title.js

---
+++ src/Components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logoDark from '../assets/shared/desktop/logo-dark.png';
+
+export default function Title() {
+  return (
+    <div>
+      <img src={logoDark} alt="Designo Logo" />
+      Our Company
+      Locations
+      Contact
+    </div>
+  )
+}
