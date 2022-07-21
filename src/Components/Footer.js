5178003127145e6ad5f42ecc657a333c5f32f597
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-07-21 12:03:38 -0400

    created Footer.js

---
+++ src/Components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logoLight from '../assets/shared/desktop/logo-light.png';
+
+export default function Footer() {
+  return (
+    <div>
+    <img src={logoLight} alt="Designo Logo" />
+    Our company
+    Locations
+    Contact
+  
+    Designo Central Office
+    3886 Wellington Street
+    Toronto, Ontario M9C 3J5
+  
+    Contact Us (Central Office)
+    P : +1 253-863-8967
+    M : contact@designo.co
+    </div>
+  )
+}
