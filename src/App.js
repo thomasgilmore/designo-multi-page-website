673b8347b03a0ec852d5026f9b3d7339b1588cc6
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-07-21 12:15:17 -0400

    setup Home route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Title from './Components/Title';
+import About from './Components/About';
+import AppDesign from './Components/AppDesign';
+import Contact from './Components/Contact';
+import GraphicDesign from './Components/GraphicDesign';
+import Home from './Components/Home';
+import Locations from './Components/Locations';
+import WebDesign from './Components/WebDesign';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div>
-      Test
+      <Title />
+      <Routes>
+        <Route path='*' element={<Home />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
