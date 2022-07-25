d86bf7189fd6390b5c03f35ab961133f34e2f61f
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-07-25 15:55:44 -0400

    working on Title.js

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -3,11 +3,13 @@ import logoDark from '../assets/shared/desktop/logo-dark.png';
 
 export default function Title() {
   return (
-    <div className='flex'>
-      <img src={logoDark} alt="Designo Logo" />
-      Our Company
-      Locations
-      Contact
+    <div className='flex m-5 justify-between items-center'>
+      <img src={logoDark} style={{ width: 250 }} alt="Designo Logo" />
+      <span>
+        Our Company
+        Locations
+        Contact
+      </span>
     </div>
   )
 }
