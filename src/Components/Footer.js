c7675ec2c1258dc830319e76149af87217d7cf38
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-07-25 20:47:51 -0400

    working on Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,19 +3,26 @@ import logoLight from '../assets/shared/desktop/logo-light.png';
 
 export default function Footer() {
   return (
-    <div>
-    <img src={logoLight} alt="Designo Logo" />
-    Our company
-    Locations
-    Contact
-  
-    Designo Central Office
-    3886 Wellington Street
-    Toronto, Ontario M9C 3J5
-  
-    Contact Us (Central Office)
-    P : +1 253-863-8967
-    M : contact@designo.co
+    <div className='m-5'>
+      <div className='flex justify-between items-center'>
+      <img src={logoLight} style={{ width: 250 }} alt="Designo Logo" />
+      Our Company
+      Locations
+      Contact
+      </div>
+
+      <div className='flex'>
+        <div className='flex flex-col'>
+          <span>Designo Central Office</span>
+          <span>3886 Wellington Street</span>
+          <span>Toronto, Ontario M9C 3J5</span>
+        </div>
+        <div className='flex flex-col'>
+          <span>Contact Us (Central Office)</span>
+          <span>P : +1 253-863-8967</span>
+          <span>M : contact@designo.co</span>
+        </div>
+      </div>
     </div>
   )
 }
