3486ac56dc744a51b2d48ad193897a4caed05c7e
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-07-25 21:20:00 -0400

    fixed Title.js

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -5,11 +5,11 @@ export default function Title() {
   return (
     <div className='flex m-5 justify-between items-center'>
       <img src={logoDark} style={{ width: 250 }} alt="Designo Logo" />
-      <span>
-        Our Company
-        Locations
-        Contact
-      </span>
+      <div className='flex gap-6'>
+        <span>Our Company</span>
+        <span>Locations</span>
+        <span>Contact</span>
+      </div>
     </div>
   )
 }
