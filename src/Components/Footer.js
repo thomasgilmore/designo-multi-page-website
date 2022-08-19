9d5c45956ccfb2c4281447e59b02811cc9046134
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-08-18 21:46:51 -0400

    footer.js fixed with react bootstrap and css

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,8 @@
-import React, { useContext } from 'react';
-import { DesignoContext } from '../Context/Context';
+import './footer.css';
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import logoLight from '../assets/shared/desktop/logo-light.png';
 import iconFacebook from '../assets/shared/desktop/icon-facebook.svg';
 import iconInstagram from '../assets/shared/desktop/icon-instagram.svg';
@@ -8,46 +11,41 @@ import iconTwitter from '../assets/shared/desktop/icon-twitter.svg';
 import iconYouTube from '../assets/shared/desktop/icon-youtube.svg';
 
 export default function Footer() {
-  const { windowSize } = useContext(DesignoContext);
-
   return (
-    <div className='p-5' style={{ backgroundColor: '#1D1C1E' }}>
-      <div className='flex justify-between items-center'>
-        <img src={logoLight} style={{ width: 250 }} alt="Designo Logo" />
-        {windowSize.innerWidth > 640 ? <div className='flex gap-6' style={{ color: '#fff' }}>
-          <span>Our Company</span>
-          <span>Locations</span>
-          <span>Contact</span>
-        </div> : null }
+    <span>
+      <Navbar expand="lg" className='footerNav' variant="dark">
+        <Container className='footerTitle'>
+          <Navbar.Brand href="#home">
+            <img src={logoLight} style={{ width: 250 }} alt="Designo Logo" />
+          </Navbar.Brand>
+            <Nav className="footerNavItems">
+              <Nav.Link href="#home">Our Company</Nav.Link>
+              <Nav.Link href="#link">Locations</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+        </Container>
+      </Navbar>
+      <div className='footerNav'>
+        <Container className='footerTextContainer'>
+          <div className='footerText'>
+            <p>Designo Central Office</p>
+            <p>3886 Wellington Street</p>
+            <p>Toronto, Ontario M9C 3J5</p>
+          </div>
+          <div className='footerText'>
+            <p>Contact Us (Central Office)</p>
+            <p>P : +1 253-863-8967</p>
+            <p>M : contact@designo.co</p>
+          </div>
+          <div className='footerSocial'>
+            <img src={iconFacebook} style={{ width: 30, height: 30 }} alt="Facebook Icon" />
+            <img src={iconYouTube} style={{ width: 30, height: 30 }} alt="YouTube Icon" />
+            <img src={iconTwitter} style={{ width: 30, height: 30 }} alt="Twitter Icon" />
+            <img src={iconPinterest} style={{ width: 30, height: 30 }} alt="Pinterest Icon" />
+            <img src={iconInstagram} style={{ width: 30, height: 30 }} alt="Instagram Icon" />
+          </div>
+        </Container>
       </div>
-
-      <hr style={{ color: '#808080', marginTop: 30, marginBottom: 30 }} />
-
-      <div className='flex flex-col text-center gap-4 justify-between sm:flex-row sm:text-left' style={{ color: "#D3D3D3" }}>
-      {windowSize.innerWidth < 640 ? 
-        <div className='flex flex-col gap-6' style={{ color: '#fff' }}>
-          <span>Our Company</span>
-          <span>Locations</span>
-          <span>Contact</span>
-        </div> : null }
-        <div className='flex flex-col'>
-          <span>Designo Central Office</span>
-          <span>3886 Wellington Street</span>
-          <span>Toronto, Ontario M9C 3J5</span>
-        </div>
-        <div className='flex flex-col'>
-          <span>Contact Us (Central Office)</span>
-          <span>P : +1 253-863-8967</span>
-          <span>M : contact@designo.co</span>
-        </div>
-        <div className='flex gap-4 items-end mx-auto my-4 sm:m-2'>
-          <img src={iconFacebook} style={{ width: 30, height: 30 }} alt="Facebook Icon" />
-          <img src={iconYouTube} style={{ width: 30, height: 30 }} alt="YouTube Icon" />
-          <img src={iconTwitter} style={{ width: 30, height: 30 }} alt="Twitter Icon" />
-          <img src={iconPinterest} style={{ width: 30, height: 30 }} alt="Pinterest Icon" />
-          <img src={iconInstagram} style={{ width: 30, height: 30 }} alt="Instagram Icon" />
-        </div>
-      </div>
-    </div>
+    </span>
   )
 }
