e4f3f9e7bf785345aa7a20f3142eec822f8a99b5
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-08-18 21:46:26 -0400

    Title.js fixed with react bootstrap and title.css

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,35 +1,26 @@
-import React, { useContext } from 'react';
-import { DesignoContext } from '../Context/Context';
+import './title.css';
+import React from 'react';
 import logoDark from '../assets/shared/desktop/logo-dark.png';
-import iconClose from '../assets/shared/mobile/icon-close.svg';
-import iconHamburger from '../assets/shared/mobile/icon-hamburger.svg';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 export default function Title() {
-  const { windowSize, mobileNavOpen, handleMobileNavOpen } = useContext(DesignoContext);
-
   return (
-    <span>
-      <div className='flex m-5 justify-between items-center'>
-        <img src={logoDark} style={{ width: 250 }} alt="Designo Logo" />
-        {windowSize.innerWidth > 640 ? <div className='flex gap-6'>
-          <span>Our Company</span>
-          <span>Locations</span>
-          <span>Contact</span>
-        </div> 
-        : <button onClick={handleMobileNavOpen}>
-            {mobileNavOpen ? 
-              <img src={iconClose} alt="Close Icon" /> 
-              : <img src={iconHamburger} alt="Hamburger Icon" />}
-          </button>
-        }
-      </div>
-      {mobileNavOpen ? 
-        <div className='flex flex-col text-center gap-6'>
-          <span>Our Company</span>
-          <span>Locations</span>
-          <span>Contact</span>
-        </div> 
-      : null }
-    </span>
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={logoDark} style={{ width: 250 }} alt="Designo Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="navItems">
+            <Nav.Link href="#home">Our Company</Nav.Link>
+            <Nav.Link href="#link">Locations</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
