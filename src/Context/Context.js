dd3069eba7e3efb0094046dd6ca3b2e0f3bf39b6
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-08-22 17:24:49 -0400

    removed context that wasn't needed

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -3,38 +3,9 @@ import React, { createContext, useState, useEffect } from "react";
 const DesignoContext = createContext();
 
 const DesignoContextProvider = ({ children }) => {
-  const [mobileNavOpen, setMobileNavOpen] = useState(false);
-
-  const handleMobileNavOpen = () => {
-    setMobileNavOpen(!mobileNavOpen);
-  }
-
-  const getWindowSize = () => {
-    const {innerWidth, innerHeight} = window;
-    return {innerWidth, innerHeight};
-  }
-  
-  const [windowSize, setWindowSize] = useState(getWindowSize())
-
-  useEffect(() => {
-    function handleWindowResize() {
-      setWindowSize(getWindowSize());
-    }
-
-    window.addEventListener('rezise', handleWindowResize);
-
-    return () => {
-      window.removeEventListener('resize', handleWindowResize);
-    };
-  }, [])
-
 
   const value = {
-    windowSize, 
-    setWindowSize,
-    mobileNavOpen,
-    setMobileNavOpen,
-    handleMobileNavOpen
+    
   }
   return (
     <DesignoContext.Provider value={value}>
