db1082f0328dbf290e59983bcc8b137026a9d700
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-08-22 10:20:37 -0400

    App Design Card

--- src/Components/AppDesignCard.js
+++ src/Components/AppDesignCard.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import './designcards.css';
 
 export default function AppDesignCard() {
   return (
     <React.Fragment>
-      <div className='appDesign'>
+      <div className='appDesignCard'>
         <h1>App Design</h1>
         <p>View Projects</p>
       </div>
