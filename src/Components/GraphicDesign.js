940a17d13b8e87ff40034cb29595fd874e33d10a
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-08-24 23:45:34 -0400

    GraphicDesign.js

--- src/Components/GraphicDesign.js
+++ src/Components/GraphicDesign.js
@@ -1,30 +1,38 @@
-import React from 'react'
+import './graphicdesign.css';
+import React, { useContext } from 'react';
+import { DesignoContext } from '../Context/Context';
+import Container from 'react-bootstrap/Container';
+import bgPatternIntroGraphic from '../assets/graphic-design/desktop/bg-pattern-intro-graphic.svg';
+import Card from './Card';
+import AppDesignCard from './AppDesignCard';
+import WebDesignCard from './WebDesignCard';
+import GetInTouch from './GetInTouch';
 
 export default function GraphicDesign() {
+  const { graphicDesignCards } = useContext(DesignoContext);
   return (
     <div>
-    Graphic design
-    We deliver eye-catching branding materials that are tailored to meet your business objectives.
+      <Container>
+      <div className='graphicDesignDiv'>
+        <h1>Graphic Design</h1>
+        <p>We deliver eye-catching branding materials that are</p> 
+        <p>tailored to meet your business objectives.</p>
+        <img src={bgPatternIntroGraphic} className="bgPatternIntroGraphic" alt="Background Pattern Intro Graphic" />
+      </div>
+
+      <div className='graphicDesignCardsDiv'>
+        {graphicDesignCards.map((card) => {
+          return <Card key={card.title} img={card.img} title={card.title} text1={card.text1} text2={card.text2} />
+        })}
+      </div>
   
-    Tim Brown
-    A book cover designed for Tim Brown’s new release, ‘Change’
-  
-    Boxed water
-    A simple packaging concept made for Boxed Water
-  
-    Science!
-    A poster made in collaboration with the Federal Art Project
-  
-    App Design
-    View Projects
-  
-    Web Design
-    View Projects
-  
-    Let’s talk about your project
-    Ready to take it to the next level? Contact us today and find out how our expertise can 
-    help your business grow.
-    Get in touch
+      <div className='appDesignAndWebDesign'>
+        <AppDesignCard />
+        <WebDesignCard />
+      </div>
+
+    </Container>
+    <GetInTouch />
   </div>
   )
 }
