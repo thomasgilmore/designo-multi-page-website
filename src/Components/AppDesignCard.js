d765095711ea04742d98ca7fc6bef6b67eb59ff5
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-08-24 23:32:39 -0400

    Added Link to AppDesignCard

--- src/Components/AppDesignCard.js
+++ src/Components/AppDesignCard.js
@@ -1,13 +1,16 @@
-import React from 'react';
 import './designcards.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function AppDesignCard() {
   return (
     <React.Fragment>
-      <div className='appDesignCard'>
-        <h1>App Design</h1>
-        <p>View Projects</p>
-      </div>
+      <Link to="/app-design" className='appDesignCard'>
+        <div className='appDesignCard'>
+          <h1>App Design</h1>
+          <p>View Projects</p>
+        </div>
+      </Link>
     </React.Fragment>
   )
 }
