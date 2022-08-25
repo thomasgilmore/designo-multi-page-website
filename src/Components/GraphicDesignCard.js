d2b3dd0b65f2616c924e2dd1df5e5d11530a5eae
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-08-24 23:32:20 -0400

    Added Link to GraphicDesignCards

--- src/Components/GraphicDesignCard.js
+++ src/Components/GraphicDesignCard.js
@@ -1,13 +1,16 @@
-import React from 'react';
 import './designcards.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function GraphicDesignCard() {
   return (
     <React.Fragment>
-      <div className='graphicDesignCard'>
-        <h1>Graphic Design</h1>
-        <p>View Projects</p>
-      </div>
+      <Link to="/graphic-design" className='graphicDesignCard'>
+        <div className='graphicDesignCard'>
+          <h1>Graphic Design</h1>
+          <p>View Projects</p>
+        </div>
+      </Link>
     </React.Fragment>
   )
 }
