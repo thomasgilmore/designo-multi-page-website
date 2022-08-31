ca449e561b67f43543a93e6629b5fa3e9827dd76
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-08-31 10:54:58 -0400

    put reused code into Places.js Component

--- src/Components/About.js
+++ src/Components/About.js
@@ -11,10 +11,7 @@ import imageWorldClassTalentDesktop from '../assets/about/desktop/image-world-cl
 import imageWorldClassTalentMobile from '../assets/about/mobile/image-world-class-talent.jpg';
 import imageWorldClassTalentTablet from '../assets/about/tablet/image-world-class-talent.jpg';
 import bgPatternThreeCircles from '../assets/shared/desktop/bg-pattern-three-circles.svg';
-import bgPatternSmallCirlce from '../assets/shared/desktop/bg-pattern-small-circle.svg';
-import illustrationAustralia from '../assets/shared/desktop/illustration-australia.svg';
-import illustrationCanada from '../assets/shared/desktop/illustration-canada.svg';
-import illustrationUnitedKingdom from '../assets/shared/desktop/illustration-united-kingdom.svg';
+import Places from './Places';
 import imageRealDealDesktop from '../assets/about/desktop/image-real-deal.jpg';
 import imageRealDealMobile from '../assets/about/mobile/image-real-deal.jpg';
 import imageRealDealTablet from '../assets/about/tablet/image-real-deal.jpg';
@@ -59,30 +56,7 @@ export default function About() {
       </div>
     </div>
 
-    <div className='placesDiv'>
-  
-      <div className='placeCanda'>
-        <img src={illustrationCanada} alt="Canada" className='illustrationCanada' />
-        <img src={bgPatternSmallCirlce} alt="bgPatternSmallCirlce" className='bgPatternSmallCirlce' />
-        <h4>Canada</h4>
-        <button className='seeLocationButton'>See location</button>
-      </div>
-
-      <div className='placeAustralia'>
-        <img src={illustrationAustralia} alt="Australia" className='illustrationAustralia' />
-        <img src={bgPatternSmallCirlce} alt="bgPatternSmallCirlce" className='bgPatternSmallCirlce' />
-        <h4>Australia</h4>
-        <button className='seeLocationButton'>See location</button>
-      </div>
-
-      <div className='placeUnitedKingdom'>
-        <img src={illustrationUnitedKingdom} alt="United Kingdom" className='illustrationUnitedKingdom' />
-        <img src={bgPatternSmallCirlce} alt="bgPatternSmallCirlce" className='bgPatternSmallCirlce' />
-        <h4>United Kingdom</h4>
-        <button className='seeLocationButton'>See location</button>
-      </div>
-
-    </div>
+    <Places />
   
     <div className='theRealDealContainer'>
       <div className='theRealDealDiv'>
